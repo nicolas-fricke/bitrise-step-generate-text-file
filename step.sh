ed35e2d163e89f076e148f9be2342b79cddbf87a
Nicolas Fricke <nicolas.fricke@devex.com>
2017-02-10 12:42:59 +0100

    Allow method calls in text body

--- step.sh
+++ step.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 file_path="`pwd`/${file_name}"
-echo "$file_content" > $file_path
+eval "echo $file_content" > $file_path
 envman add --key GENERATED_TEXT_FILE_PATH --value $file_path
 exit 0
