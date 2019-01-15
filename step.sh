64bff0968fb44bf265e40764fa2dfa966cc4bbfd
Viktor Benei <viktor.benei@gmail.com>
2019-01-15 17:54:40 +0100

    Rewrite in Go

--- step.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-file_path="`pwd`/${file_name}"
-eval "echo \"$file_content\"" > $file_path
-printf "> cat $file_path\n\n"
-cat $file_path
-printf "\n\nexporting GENERATED_TEXT_FILE_PATH=$file_path\n"
-envman add --key GENERATED_TEXT_FILE_PATH --value $file_path
-exit 0
